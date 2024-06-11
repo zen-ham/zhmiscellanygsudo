40a123449c3b1761c358db2afe40000392619cda
Your Name <again.really.plz@gmail.com>
2024-06-12 05:20:10 +1000

    Automated commit from zen-ham at 2024-06-12 05:20:10.503008

--- src/zhmiscellanygsudo/_supportfuncs.py
+++ src/zhmiscellanygsudo/_supportfuncs.py
@@ -6,6 +6,7 @@ def get_gsudo_binary_path():
     if getattr(sys, 'frozen', False):
         # we are running in a PyInstaller bundle
         base_path = sys._MEIPASS
+        anyway = True
     else:
         # we are running in normal Python environment
         base_path = os.path.dirname(__file__)
@@ -20,8 +21,6 @@ def get_gsudo_binary_path():
 
     cwd = os.getcwd()
     if (not os.path.exists(os.path.join(base_path, 'resources'))) or anyway:
-        if os.path.exists(os.path.join(base_path, 'resources')):
-            zhmiscellany.fileio.remove_folder(os.path.join(base_path, 'resources'))
         os.chdir(base_path)
         from ._py_resources import gen
         gen()
