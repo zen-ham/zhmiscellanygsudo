b8de16bd05ba892b2c92c3b4ee76b5efa31232b8
Your Name <again.really.plz@gmail.com>
2024-06-12 06:14:10 +1000

    Automated commit from zen-ham at 2024-06-12 06:14:10.897045

--- src/zhmiscellanygsudo/admin_subprocess.py
+++ src/zhmiscellanygsudo/admin_subprocess.py
@@ -2,7 +2,7 @@ from ._supportfuncs import _gsudo_binary_path
 import subprocess
 
 
-def Popen(command, keep_same_console=True, **kwargs):
+def run(command, keep_same_console=True, **kwargs):
     if type(command) == list:
         command.insert(0, _gsudo_binary_path)
         if not keep_same_console:
@@ -10,6 +10,17 @@ def Popen(command, keep_same_console=True, **kwargs):
     elif type(command) == str:
         command = f'{_gsudo_binary_path}{" -n" if not keep_same_console else ""} {command}'
     else:
+        raise TypeError(f'Invalid command type "{type(command)}" for zhmiscellanygsudo.run, requires list or str')
+    process = subprocess.run(command, **kwargs)
+    return process
+
+
+def Popen(command, **kwargs):
+    if type(command) == list:
+        command.insert(0, _gsudo_binary_path)
+    elif type(command) == str:
+        command = f'{_gsudo_binary_path} {command}'
+    else:
         raise TypeError(f'Invalid command type "{type(command)}" for zhmiscellanygsudo.Popen, requires list or str')
-    process = subprocess.Popen(command, **kwargs)
+    process = subprocess.run(command, **kwargs)
     return process
